7d08a62780ed13d6af9c33658c98e1a2221e38c6
Deniz Hacısalihoğlu <denizhsalihoglu@gmail.com>
2023-01-09 19:48:38 +0300

    Add logo to sidebar

--- pages/components/sidebar.tsx
+++ pages/components/sidebar.tsx
@@ -1,3 +1,4 @@
+import NextImage from 'next/image'
 import {
     Box,
     List,
@@ -18,8 +19,14 @@ import {
 } from 'react-icons/md'
 
 const Sidebar = () => {
-    return(
-        <div> Hello from sidebar</div>
+    return (
+        <Box width="100%" height="calc(100vh - 100px)" bg="black" paddingX="5px" color="gray">
+            <Box paddingY="25px">
+                <Box width="170px" marginBottom="20px" paddingX="20px">
+                    <NextImage src="/logo.png" height={72} width={240}/>
+                </Box>
+            </Box>
+        </Box>
     )
 }
 
